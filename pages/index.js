80e3baada6aab5fc566ecb5c8a86a23070c23f65
Fahim Zada <programmingway1@gmail.com>
2021-08-26 02:33:43 +0500

    meta tags added to head

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,17 @@ const HOME = () =>
   return (
     <div>
       <Head>
-        <title>Itfz1 Porfolio! - Welcome</title>
+        <title>Fahim Zada (Itfz1) Porfolio! - Welcome</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="Fahim Zada"></meta>
+        <meta name="description" content='Web App Lover who use effective and optimize approach to solve real world problems. Cant' />
+        <meta name="keywords" content='Fahim Zada, itsfz1, red apple, fahim zada web developer, fahim zada software engineer' />
+        <meta property="og:image" content='/images/itsfz1.png' />
+        <meta property="og:description" content='Web App Lover who use effective and optimize approach to solve real world problems. Cant' />
+        <meta property="og:title" content='Fahim Zada (Itfz1) Porfolio! - Welcome' />
+        <meta property="twitter:image" content='/images/itsfz1.png' />
+        <meta property="twitter:title" content='Fahim Zada (Itfz1) Porfolio! - Welcome' />
+        <meta property="twitter:description" content='Web App Lover who use effective and optimize approach to solve real world problems. Cant' />
       </Head>
       <Header />
       <Education />
