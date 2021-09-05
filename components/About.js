70165dacb3c8ce19136e3ade515f37387bdfde8b
Fahim Zada <programmingway1@gmail.com>
2021-09-05 20:24:05 +0500

    Title added to icons

--- components/About.js
+++ components/About.js
@@ -35,30 +35,30 @@ export const About = () =>
                     </p>
                     <h3 className="second-name">Hobbies</h3>
                     <div className="about-icons">
-                        <AiOutlineRead style={{ height: "2rem", width: "2rem", color: "#fff" }} />
-                        <GiCampCookingPot style={{ height: "2rem", width: "2rem", color: "#fff" }} />
-                        <BsMusicNoteList style={{ height: "2rem", width: "2rem", color: "#fff" }} />
-                        <CgGames style={{ height: "2rem", width: "2rem", color: "#fff" }} />
+                        <a title="Reading"><AiOutlineRead className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} /></a>
+                        <a title="Cooking"><GiCampCookingPot className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} /></a>
+                        <a title="Music"><BsMusicNoteList className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} /></a>
+                        <a title="Video Games"><CgGames className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} /></a>
                     </div>
                     <h3 className="engineer">Socials</h3>
                     <div className="about-icons">
-                        <a href="https://www.facebook.com/faheem.zada.9/" target="_blank" rel="noreferrer">
+                        <a href="https://www.facebook.com/faheem.zada.9/" target="_blank" rel="noreferrer" title="Facebook">
                             <FaFacebook className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} />
                         </a>
-                        <a href="https://www.linkedin.com/in/fahimzada/" target="_blank" rel="noreferrer">
+                        <a href="https://www.linkedin.com/in/fahimzada/" target="_blank" rel="noreferrer" title="Linkedin">
                             <FaLinkedin className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} />
                         </a>
-                        <a href="https://www.instagram.com/itsfz1/" target="_blank" rel="noreferrer">
+                        <a href="https://www.instagram.com/itsfz1/" target="_blank" rel="noreferrer" title="Instagram">
                             <FaInstagramSquare className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} />
                         </a>
-                        <a href="https://github.com/itsfz1" target="_blank" rel="noreferrer">
+                        <a href="https://github.com/itsfz1" target="_blank" rel="noreferrer" title="Github">
                             <FaGithub className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} />
                         </a>
                         <a href="https://api.whatsapp.com/send?phone=923349093082&text=Thank%20You%20For%20Contacting%20Will%20Get%20Back%20To%20You%20As%20Soon%20As%20Possible%20:)"
-                            target="_blank" rel="noreferrer">
+                            target="_blank" rel="noreferrer" title="Whatsapp">
                             <FaWhatsappSquare className="view" style={{ height: "2rem", width: "2rem", color: "#fff" }} />
                         </a>
-                        <a href="mailto:me@fahimzada.com" target="_blank" rel="noreferrer">
+                        <a href="mailto:me@fahimzada.com" target="_blank" rel="noreferrer" title="Mail">
                             <ImMail className="view" style={{ height: "1.9rem", width: "2rem", color: "#fff" }} />
                         </a>
                     </div>
